1b3eecd9f601a78368bde1672bfa5ea9c50407b0
André <andreglock@gmail.com>
2021-03-09 14:28:35 +0100

    up to interactions 01 mid day

--- classes/convert01.py
+++ classes/convert01.py
@@ -15,4 +15,16 @@ for i in range (1, 10):
     with open('0' + str(i) + 'BDL.html', "w") as modified: modified.write(head + data + foot)
     print('0' + str(i) + 'BDL.html' + ' made')
 
+for i in range (10, 31):
+    print(str('%02d' % (i)) + 'UIB.md is input', end='    ')
+    markdown.markdownFromFile(
+    input= str('%02d' % (i)) + 'UIB.md',
+    output= 'stage.html',
+    encoding='utf8',
+    )
+    with open('stage.html', 'r') as original: data = original.read()
+    with open('template.html', 'r') as template: head = template.read()
+    with open(str("%02d" % (i)) + 'UIB.html', "w") as modified: modified.write(head + data + foot)
+    print(str("%02d" % (i)) + 'UIB.html' + ' made')
+
 os.remove("stage.html") 
--- classes/convert02.py
+++ classes/convert02.py
@@ -3,16 +3,16 @@ import os
 
 foot = '\n        </div>\n    </body>\n</html>'
 
-for i in range (10, 36):
-    print(str('%02d' % (i)) + 'UIB.md is input', end='    ')
+for i in range (31, 32):
+    print(str('%02d' % (i)) + 'INT.md is input', end='    ')
     markdown.markdownFromFile(
-    input= str('%02d' % (i)) + 'UIB.md',
+    input= str('%02d' % (i)) + 'INT.md',
     output= 'stage.html',
     encoding='utf8',
     )
     with open('stage.html', 'r') as original: data = original.read()
     with open('template.html', 'r') as template: head = template.read()
-    with open(str("%02d" % (i)) + 'UIB.html', "w") as modified: modified.write(head + data + foot)
-    print(str("%02d" % (i)) + 'UIB.html' + ' made')
+    with open(str("%02d" % (i)) + 'INT.html', "w") as modified: modified.write(head + data + foot)
+    print(str("%02d" % (i)) + 'INT.html' + ' made')
 
 os.remove("stage.html") 
